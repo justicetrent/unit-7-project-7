9327bb33d6e075a284aca5b3911721445e2b06f3
Justice Trent <justice.trent@verizon.com>
2019-08-13 16:18:49 -0400

    new commit

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import apiKey from './config.js'
 
 //https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=YOURAPIKEY&format=json&nojsoncallback=1&text=cats&extras=url_o
 
