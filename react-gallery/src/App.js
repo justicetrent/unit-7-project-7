cd115e3b5b4db83b307236d7da66a6d99461225a
Justice Trent <justice.trent@verizon.com>
2019-08-13 15:15:38 -0400

    set up project 7

---
+++ react-gallery/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './index.css';
+
+//https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=YOURAPIKEY&format=json&nojsoncallback=1&text=cats&extras=url_o
+
